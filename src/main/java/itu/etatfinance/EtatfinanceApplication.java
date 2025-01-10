d4e3e60da3607e927a500362112d69cbaf64403a
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-01-10 21:54:13 +0300

    init spring boot project

---
+++ src/main/java/itu/etatfinance/EtatfinanceApplication.java
@@ -0,0 +1,13 @@
+package itu.etatfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtatfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtatfinanceApplication.class, args);
+	}
+
+}
